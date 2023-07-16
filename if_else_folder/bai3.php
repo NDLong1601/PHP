b9e738b916951d452efe1de016a22fccccc5514d
NguyenLong <nguyenlong160104@gmail.com>
2023-07-16 18:04:07 +0700

    If ELse

---
+++ if_else_folder/bai3.php
@@ -0,0 +1,8 @@
+<?php
+   $year = 2003;
+   if (($year % 400 == 0) || ($year % 4 == 0 && $year % 100 > 0)) {
+     echo 'Đây là năm nhuận';
+   } else {
+     echo 'Đây không phải năm nhuận';
+   }
+ ?>
