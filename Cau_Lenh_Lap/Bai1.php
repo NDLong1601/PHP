ab4f97360e9a7891659c6db67c56c63c4ffa19e5
NguyenLong <nguyenlong160104@gmail.com>
2023-07-16 18:16:56 +0700

    vong lap

---
+++ Cau_Lenh_Lap/Bai1.php
@@ -0,0 +1,28 @@
+<?php
+
+   for ($i = 0;$i < 10;$i ++) {
+     echo "<br>";
+     for ($j = 0;$j < 20;$j ++) {
+       echo "*";
+     }
+     
+   };
+   echo "<br>";
+   for ($x = 0;$x < 10;$x ++) {
+     for ($y =(10-$x);$y < 10;$y ++) {
+         echo "*";
+     }
+     echo "<br>";
+     
+   };
+   echo "<br>";
+   echo "<br> ****   **** ";
+   echo "<br>*************";
+   echo "<br>*************";
+   echo "<br> *********** ";
+   echo "<br>  *********  ";
+   echo "<br>   *******   ";
+   echo "<br>     ***     ";
+   echo "<br>      *      ";
+
+ ?>
