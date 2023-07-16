26925137fd745f13b81c01eeee1e7758c2671707
NguyenLong <nguyenlong160104@gmail.com>
2023-07-16 18:29:42 +0700

    Gioi thieu PHP

---
+++ Gioi_Thieu_PHP/GioiThieu.php
@@ -0,0 +1,16 @@
+<?php
+$arr = array("Traning","PHP",150,True,3.14);
+$bool = True;
+print_r($arr);
+echo "<br>";
+var_dump($bool);
+echo "<br>";
+echo $bool;
+echo "<br>";
+var_dump($arr);
+/* 
+Khi cần in thông tin của 1 biến thì dùng print_r()
+Khi biết về giá trị và kiểu dữ liệu thì dùng var_dump()
+echo có thể hiện 1 lúc nhiều giá trị,echo nhanh hơn print
+*/
+?>
