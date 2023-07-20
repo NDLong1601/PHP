c79dbfdb4b0067618ff51b5370cea4b39dc8aec0
NguyenLong <nguyenlong160104@gmail.com>
2023-07-20 13:34:26 +0700

    array

--- array_folder/Bai1.php
+++ array_folder/Bai1.php
@@ -1,9 +1,16 @@
 <?php
-    //Viết chương trình tạo 1 mảng số nguyên bất kỳ và tìm in ra số lớn nhất và nhỏ nhất trong mảng
-    $array = [4,2,5,1,9,10,99,2];
-    // for ($i=0;) {
+//Viết chương trình tạo 1 mảng số nguyên bất kỳ và tìm in ra số lớn nhất và nhỏ nhất trong mảng
+$length = 10;
+$min = 1;
+$max = 100;
 
-    // }
-    echo "Max la:".max($array);
-    echo "<br>Min la: ".min($array);
+$numbers = array();
+
+for ($i = 0; $i < $length; $i++) {
+  $randomNumber = mt_rand($min, $max);
+  $numbers[] = $randomNumber;
+  echo "<br>";
+}
+print_r($numbers);
+
 ?>
